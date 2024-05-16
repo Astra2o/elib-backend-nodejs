2f7c199513b6502aba311356eee979dac3ad8ac0
amanpal <amanpalsambhalkha@gmail.com>
2024-05-16 13:54:15 +0530

    setup express

--- server.ts
+++ server.ts
@@ -1,1 +1,17 @@
-console.log('welcome');
+import app from "./src/app"; 
+
+
+
+
+
+
+
+const startServer=()=>{
+    const port = process.env.PORT || 3000;
+
+    app.listen(port,()=>{
+        console.log(`listening on port : ${port}`)
+    })
+}
+
+startServer();
---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express()
+
+// endpoint - ROUTES
+// http methods - get,post,put,patch,delete
+
+app.get('/',(req,res,next)=>{
+    res.json({message:'welcome'})
+})
+
+
+
+export default app;
