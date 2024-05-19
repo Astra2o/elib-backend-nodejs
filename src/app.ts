6d3480e0e0c463e87bdd9ca28124bdea09a5a780
amanpal <amanpalsambhalkha@gmail.com>
2024-05-19 15:11:20 +0530

    CREATE user router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response ,NextFunction} from "express";
 import createHttpError,{HttpError} from 'http-errors'
 import { conf } from "./config/config";
 import globalErrorHandler from "./middleware/globalErrorHandler";
+import userRouter from "./user/userRouter";
 // import { create } from "domain";
 // import { error } from "console";
 const app = express()
@@ -10,13 +11,13 @@ const app = express()
 // http methods - get,post,put,patch,delete
 
 app.get('/',(req,res,next)=>{
-    const error = createHttpError(400,'something went wrong')
-    throw error
+    // const error = createHttpError(400,'something went wrong')
+    // throw error
     res.json({message:'welcome'})
 })
 
 
-
+app.use('/api/users',userRouter)
 
 app.use(globalErrorHandler)
 
