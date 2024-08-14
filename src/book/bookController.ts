a030b647da1a56c610dc2cc1a3d807456d8f316a
amanpal <amanpalsambhalkha@gmail.com>
2024-08-14 13:59:00 +0530

    error solved in cloudnary upload and upload the file sucessfully

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,7 +1,20 @@
 import { NextFunction,Request,Response } from "express";
 // import createHttpError from "http-errors";
-import cloudinary from "../config/cloudinary";
+import {v2 as cloudinary} from 'cloudinary';
+import { conf } from "../config/config";
+
+
+
+    // Configuration
+ export  default  cloudinary.config({ 
+        cloud_name: conf.cloudName, 
+        api_key: conf.cloudApiKey, 
+        api_secret:conf.cloudApiSecret 
+    });
+
 import path from "path";
+import { log } from "console";
+import createHttpError from "http-errors";
 // import { error, log } from "console";
 const createBook = async (req:Request,res:Response,next:NextFunction) => {
     // res.json({msg:'sucess book added'})
@@ -16,15 +29,36 @@ const createBook = async (req:Request,res:Response,next:NextFunction) => {
         const filename=files.coverImg[0].filename;
         const filepath = path.resolve(__dirname,'../../public/data/uploads',filename)
         console.log(filepath);
+
+        const bookFileName = files.file[0].filename;
+        const bookFilepath = path.resolve(__dirname,'../../public/data/uploads',bookFileName)
+       
+
+        try {
+            const uploadResult =await cloudinary.uploader        // here is an error 
+            .upload(filepath,{                                    //  error TypeError: Cannot read properties of undefined (reading 'upload')
+                filename_override:filename,
+                // public_id:'bookcover',
+                folder:'book-cover',
+                format:coverImageMimeType 
+            })
+
+            const uploadFileResult =await cloudinary.uploader        // here is an error 
+            .upload(bookFilepath,{
+                resource_type:'raw',                                    //  error TypeError: Cannot read properties of undefined (reading 'upload')
+                filename_override:bookFileName,
+                // public_id:'books',
+                folder:'book-file',
+                format:'pdf' 
+            })
+             console.log(uploadResult,uploadFileResult);
+        } catch (error) {
+            console.log(error);
+            return next(createHttpError(500,'error while uploading the files'))
+            
+        }
         
-//         const uploadResult =await cloudinary.uploader        // here is an error 
-//         .upload(filepath,{                                    //  error TypeError: Cannot read properties of undefined (reading 'upload')
-//             filename_override:filename,
-//             public_id:'bookcover',
-//             folder:'book-cover',
-//             format:coverImageMimeType 
-//         }) .then((result: any)=>console.log(result))
-// console.log(uploadResult);
+        
         res.send('sucess')
     } catch (error) {
         console.log('error', error); 
--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,14 +1,14 @@
-import {v2 as cloudinary} from 'cloudinary';
-import { conf } from './config';
+// import {v2 as cloudinary} from 'cloudinary';
+// import { conf } from './config';
 
 
 
-    // Configuration
- export  default  cloudinary.config({ 
-        cloud_name: conf.cloudName, 
-        api_key: conf.cloudApiKey, 
-        api_secret:conf.cloudApiSecret 
-    });
+//     // Configuration
+//  export  default  cloudinary.config({ 
+//         cloud_name: conf.cloudName, 
+//         api_key: conf.cloudApiKey, 
+//         api_secret:conf.cloudApiSecret 
+//     });
 
 
-    
+    // not working then i copy paste in bookcontroller
